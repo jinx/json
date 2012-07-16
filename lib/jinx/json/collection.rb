05093db6ed568254d7c73109caa69fd2a0706e9d
Fred Loney <loneyf@ohsu.edu>
2012-07-16 14:23:27 -0700

    Support top-level collection serialization.

--- lib/jinx/json/collection.rb
+++ lib/jinx/json/collection.rb
@@ -1,13 +1,16 @@
 require 'jinx/helpers/collection'
+require 'jinx/json/state'
 
 module Jinx
   module Collection
     # Adds JSON serialization to collections.
     #
-    # @param args the JSON serialization options
-    # @return [String] the JSON representation of this Enumerable as an array
-    def to_json(*args)
-      to_a.to_json(*args)
+    # @param [State, Hash, nil] state the JSON state or serialization options
+    # @return [String] the JSON representation of this {Jinx::Resource}
+    def to_json(state=nil)
+      # Make a new State from the options if this is a top-level call.
+      state = JSON::State.for(state) unless JSON::State === state
+      to_a.to_json(state)
     end
   end
 end
