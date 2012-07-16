6af821226a46e5f8c550e2b1448c94c0aa56a289
Fred Loney <loneyf@ohsu.edu>
2012-07-16 14:23:40 -0700

    Disable lazy-loader.

--- lib/jinx/json/serializable.rb
+++ lib/jinx/json/serializable.rb
@@ -77,7 +77,8 @@ module Jinx
           return {:object_id => proxy_object_id}
         end
         visited << self
-        vh = value_hash
+        # Turn off the lazy loader when making the attribute => value hash.
+        vh = do_without_lazy_loader { value_hash }
         vh.each { |pa, v| vh[pa] = v.to_a if Set === v } 
         # Add the object id.
         vh[:object_id] = proxy_object_id
